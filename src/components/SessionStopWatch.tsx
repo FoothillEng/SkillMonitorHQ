d0ecf7118ee29a545a44d8b34293d7572a567fa1
0x6F72656F73 <godbobz12@gmail.com>
2023-09-14 10:21:02 -0700

    fix: session timer stop => time 0

--- src/components/SessionStopWatch.tsx
+++ src/components/SessionStopWatch.tsx
@@ -77,6 +77,7 @@ const SessionStopWatch = ({
         if (intervalIDRef.current !== null) {
             clearInterval(intervalIDRef.current);
             intervalIDRef.current = null;
+            setTime(0);
         }
     }, []);
 
