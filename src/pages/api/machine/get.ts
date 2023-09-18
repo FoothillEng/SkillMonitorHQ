cdf52e5f8848cb67f1d62508278a255d4fdf3a74
0x6F72656F73 <godbobz12@gmail.com>
2023-09-18 13:43:55 -0700

    fix: api handle machine not found

--- src/pages/api/machine/get.ts
+++ src/pages/api/machine/get.ts
@@ -21,10 +21,11 @@ export default async function handler(
                             res.status(404).json({
                                 message: 'Machine not found'
                             });
+                        } else {
+                            res.status(200).json({
+                                machine
+                            });
                         }
-                        res.status(200).json({
-                            machine
-                        });
                     })
                 } else {
                     res.status(404).json({
