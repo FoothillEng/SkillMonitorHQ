542d5bbbdba10bae64d4a2fba28c58babe759f25
0x6F72656F73 <godbobz12@gmail.com>
2023-09-27 12:57:50 -0700

    fix: admin middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,10 @@ import { withAuth } from "next-auth/middleware"
 
 export default withAuth({
     callbacks: {
-        authorized({ req, token }) {
-            // `/admin` requires admin role
-            if (req.nextUrl.pathname === "/admin") {
+        authorized({ req, token }) {            // `/admin` requires admin role
+            if (req.nextUrl.pathname.startsWith("/admin") || req.nextUrl.pathname.startsWith("/api/admin")) {
                 return token?.admin === true
             }
-
-            // `/me` only requires the user to be logged in
             return !!token
         },
     },
@@ -19,3 +16,5 @@ export const config = { matcher: ["/admin/:path*", "/api/admin/:path*"] }
 
 // for timeout logic
 // one route would be doing it possible through middleware??? probably not. safer, more reliable option is using setInterval to check if timeout has passed
+
+// make sure /admin/teacher/:path* along with /api/admin/teacher checks if user is teacher
