ee2686e68364c542a2e3b7c2a122697d74c8a5d9
0x6F72656F73 <godbobz12@gmail.com>
2023-12-15 19:04:59 -0800

    fix: font

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="mt-10 px-24 py-20">
-            <span className="block text-center text-sm text-white">
+            <span className="block text-center text-xl text-white">
                 © 2023 SkillMonitorHQ™ All Rights Reserved.
             </span>
         </footer>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,10 @@ const Header = () => {
     const { machineName } = useContext(MachineContext);
     return (
         <div className="text-fhs-blue mb-20 mt-16 w-full font-oxygen">
-            <h1 className="text-center text-9xl">
-                {machineName
-                    ? `SkillMonitorHQ - ${machineName}`
-                    : 'SkillMonitorHQ'}
-            </h1>
+            <div className="text-center text-9xl uppercase">
+                <div>SkillMonitorHQ</div>
+                {machineName && <div className="">{machineName}</div>}
+            </div>
         </div>
     );
 };
