07b096d3df1d648122c5c0df60dfcc0c23c6f9b8
0x6F72656F73 <godbobz12@gmail.com>
2023-12-17 15:34:14 -0800

    feat: dynamic autologout time

--- src/components/AutoLogoutTimer.tsx
+++ src/components/AutoLogoutTimer.tsx
@@ -10,6 +10,7 @@ import { FormattedTime } from '@/pages/index';
 const AutoLogoutTimer = () => {
     const [time, setTime] = useState(0);
     const [isOpen, setIsOpen] = useState(false);
+    const [adminSet, setAdminSet] = useState(false); // setTimeout to clear and set to false
     const [error, setError] = useState('');
 
     const { data: nextAuthSession, status, update } = useSession();
@@ -20,7 +21,6 @@ const AutoLogoutTimer = () => {
                 const timeRemaining =
                     nextAuthSession?.user.realExpTime -
                     Math.floor(Date.now() / 1000);
-                console.log(nextAuthSession?.user.realExpTime, timeRemaining);
 
                 if (timeRemaining <= 0) {
                     signOut();
@@ -44,34 +44,63 @@ const AutoLogoutTimer = () => {
         }
     }, [nextAuthSession?.user, status]);
 
-    const handleSubmit = async (
+    const handleSubmit1 = async (
         studentId: string,
         setStudentId: any,
         setError: any
     ) => {
         const adminId = parseInt(studentId);
 
+        if (isNaN(adminId)) {
+            setError('Please enter a valid number');
+            return;
+        }
+
         await fetch(`/api/checkAdmin`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ adminId })
+            body: JSON.stringify({ adminId }) // technically should be encrypted...
         })
             .then((res) => {
                 if (res.status === 401) {
                     setError('Invalid Admin ID');
                 } else {
-                    update({
-                        realExpTime: nextAuthSession?.user.realExpTime + 1800
-                    });
+                    setAdminSet(true);
                 }
-                setIsOpen(false);
             })
             .catch((error) => {
                 console.error(error);
                 setError('An error occurred. Please try again.'); // TODO: where does this error lead to
             });
+    };
+
+    const handleSubmit2 = async (
+        studentId: string,
+        setStudentId: any,
+        setError: any
+    ) => {
+        const hours = parseInt(studentId);
+
+        if (!adminSet) {
+            setError('Please enter Admin ID first');
+            return;
+        }
+
+        if (isNaN(hours)) {
+            setError('Please enter a valid number');
+            return;
+        }
+
+        if (hours < 1 || hours > 24) {
+            setError('Please enter a number between 1 and 24');
+            return;
+        }
+
+        update({
+            realExpTime: nextAuthSession?.user.realExpTime + hours * 3600
+        });
 
         setIsOpen(false);
     };
@@ -101,17 +130,29 @@ const AutoLogoutTimer = () => {
                                 className="relative z-50"
                             >
                                 <div
-                                    className="fixed inset-0 bg-black/20"
+                                    className="fixed inset-0 bg-black/80"
                                     aria-hidden="true"
                                 />
 
                                 <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
                                     <Dialog.Panel className="mx-auto flex h-[65rem] w-[50rem] items-center justify-center rounded bg-green-500 text-center">
-                                        <LockScreen
-                                            placeholder={'Enter Admin ID'}
-                                            start={''}
-                                            handleSubmit={handleSubmit}
-                                        />
+                                        {(!adminSet && (
+                                            <LockScreen
+                                                placeholder={'Enter Admin ID'}
+                                                start={''}
+                                                handleSubmit={handleSubmit1}
+                                                key={1}
+                                            />
+                                        )) || (
+                                            <LockScreen
+                                                placeholder={
+                                                    'Enter Total # of Hours'
+                                                }
+                                                start={''}
+                                                handleSubmit={handleSubmit2}
+                                                key={2}
+                                            />
+                                        )}
                                     </Dialog.Panel>
                                 </div>
                             </Dialog>
