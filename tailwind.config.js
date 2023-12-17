7f3ae44eed51844dfc03877037ac6a7c30bc846d
0x6F72656F73 <godbobz12@gmail.com>
2023-12-17 15:33:22 -0800

    feat: Add new screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+        'sm': '1100px', // vertical
+        'md': '1500px', // horizontal
+      },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
