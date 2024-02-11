4ad18fb718454e14b1cd5ea6f4b23da9a32fc159
0x6F72656F73 <godbobz12@gmail.com>
2024-02-11 05:29:48 -0800

    feat: teacher settings reorganize + test questions

---
+++ src/pages/teacher/index.tsx
@@ -0,0 +1,11 @@
+import Title from '@/components/Title';
+
+const TeacherIndex = (props) => {
+    return (
+        <div className="flex w-screen flex-col items-center text-center font-oxygen text-green">
+            <Title title="Teacher Home" />
+        </div>
+    );
+};
+
+export default TeacherIndex;
